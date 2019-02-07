e8beb92ff39abd5e78e61cf14c1b4ca909407039
Manuel Mendez <mmendez534@gmail.com>
2019-02-07 10:41:45 -0500

    update
    github.com/neovim/neovim/compare/f6faeea41c70015f6d9b63fecaf80d106cd2636d...969cc55993919530b2a96e30f02fe71711cab28a
    github.com/jwilm/alacritty/compare/153f9257b8a78432e9a7830f1eb3c539716f6724...b295ac9b9ba7299ccb7edee17e2ac0c6334be2fb
    github.com/swaywm/sway/compare/f00fbe6861bad18fc0615be8fe5ba0102c609067...e8c472aee95ce1cbb6feef04d970327d1717d7fc
    github.com/mobile-shell/mosh/compare/944fd6c796338235c4f3d8daf4959ff658f12760...c3a2756065a0fb04cfd2681280123b362d862a5e

--- pkgs/applications/window-managers/sway/beta.nix
+++ pkgs/applications/window-managers/sway/beta.nix
@@ -10,13 +10,13 @@
 stdenv.mkDerivation rec {
   name = "${pname}-${version}";
   pname = "sway";
-  version = "1.0";
+  version = "20190214g" + (builtins.substring 0 8 "${src.rev}");
 
   src = fetchFromGitHub {
     owner = "swaywm";
     repo = "sway";
-    rev = version;
-    sha256 = "09cndc2nl39d3l7g5634xp0pxcz60pvc5277mfw89r22mh0j78rx";
+    rev = "e8c472aee95ce1cbb6feef04d970327d1717d7fc";
+    sha256 = "13n4ixrhc04186nzavs57asbirxp7sp8nj1hsjj91cy9mkbjs99l";
   };
 
   nativeBuildInputs = [
