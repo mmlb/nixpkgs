e518d5fde9023ac1257c82c84bfe04c329c89405
Manuel Mendez <mmendez534@gmail.com>
2019-03-11 23:59:56 -0400

    sway: use SWAY_VERSION meson option to inject version info

--- pkgs/applications/window-managers/sway/beta.nix
+++ pkgs/applications/window-managers/sway/beta.nix
@@ -19,15 +19,6 @@ stdenv.mkDerivation rec {
     sha256 = "09cndc2nl39d3l7g5634xp0pxcz60pvc5277mfw89r22mh0j78rx";
   };
 
-  patches = [
-    # Fix for a compiler warning that causes a build failure
-    # (see https://github.com/swaywm/sway/issues/3862):
-    (fetchpatch {
-      url = "https://github.com/swaywm/sway/commit/bcde298a719f60b9913133dbd2a169dedbc8dd7d.patch";
-      sha256 = "0r583nmqvq43ib93yv6flw8pj833v32lbs0q0xld56s3rnzvvdcp";
-    })
-  ];
-
   nativeBuildInputs = [
     pkgconfig meson ninja
   ] ++ stdenv.lib.optional buildDocs scdoc;
@@ -41,7 +32,7 @@ stdenv.mkDerivation rec {
   enableParallelBuilding = true;
 
   mesonFlags = [
-    "-Dxwayland=enabled" "-Dgdk-pixbuf=enabled" "-Dtray=enabled"
+    "-Dxwayland=enabled" "-Dgdk-pixbuf=enabled" "-Dtray=enabled" "-DSWAY_VERSION=${version}"
   ] ++ stdenv.lib.optional buildDocs "-Dman-pages=enabled";
 
   meta = with stdenv.lib; {
