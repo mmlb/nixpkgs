50eed4cdaf04155914e9438f8ca50aeff6a31dea
Manuel Mendez <mmendez534@gmail.com>
2019-03-11 23:59:56 -0400

    sway: use SWAY_VERSION meson option to inject version info

--- pkgs/applications/window-managers/sway/default.nix
+++ pkgs/applications/window-managers/sway/default.nix
@@ -4,6 +4,7 @@
 , wayland, libxkbcommon, pcre, json_c, dbus, libevdev
 , pango, cairo, libinput, libcap, pam, gdk_pixbuf
 , wlroots, wayland-protocols
+, buildDocs ? true
 }:
 
 stdenv.mkDerivation rec {
@@ -18,17 +19,9 @@ stdenv.mkDerivation rec {
     sha256 = "09cndc2nl39d3l7g5634xp0pxcz60pvc5277mfw89r22mh0j78rx";
   };
 
-  patches = [
-    # Fix for a compiler warning that causes a build failure
-    # (see https://github.com/swaywm/sway/issues/3862):
-    (fetchpatch {
-      url = "https://github.com/swaywm/sway/commit/bcde298a719f60b9913133dbd2a169dedbc8dd7d.patch";
-      sha256 = "0r583nmqvq43ib93yv6flw8pj833v32lbs0q0xld56s3rnzvvdcp";
-    })
-    ./sway-config-no-nix-store-references.patch
-  ];
-
-  nativeBuildInputs = [ pkgconfig meson ninja scdoc ];
+  nativeBuildInputs = [
+    pkgconfig meson ninja
+  ] ++ stdenv.lib.optional buildDocs scdoc;
 
   buildInputs = [
     wayland libxkbcommon pcre json_c dbus libevdev
@@ -39,9 +32,8 @@ stdenv.mkDerivation rec {
   enableParallelBuilding = true;
 
   mesonFlags = [
-    "-Ddefault-wallpaper=false" "-Dxwayland=enabled" "-Dgdk-pixbuf=enabled"
-    "-Dtray=enabled" "-Dman-pages=enabled"
-  ];
+    "-Dxwayland=enabled" "-Dgdk-pixbuf=enabled" "-Dtray=enabled" "-DSWAY_VERSION=${version}"
+  ] ++ stdenv.lib.optional buildDocs "-Dman-pages=enabled";
 
   meta = with stdenv.lib; {
     description = "i3-compatible tiling Wayland compositor";
