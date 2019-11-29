abc62374b2512f04b4bbd9ee54a26c54db81607d
Manuel Mendez <mmendez534@gmail.com>
2019-11-29 08:43:31 -0500

    Use pname instead of name in mkDerivation and src.repo

--- pkgs/applications/editors/neovim/default.nix
+++ pkgs/applications/editors/neovim/default.nix
@@ -19,12 +19,12 @@ let
     ));
 in
   stdenv.mkDerivation rec {
-    pname = "neovim-unwrapped";
+    pname = "neovim";
     version = "unstable-2019-11-06g${builtins.substring 0 9 src.rev}";
 
     src = fetchFromGitHub {
       owner = "neovim";
-      repo = "neovim";
+      repo = pname;
       rev = "b99dad7b4c6418978a21977262809021fab8d356";
       sha256 = "03p7pic7hw9yxxv7fbgls1f42apx3lik2k6mpaz1a109ngyc5kaj";
     };
