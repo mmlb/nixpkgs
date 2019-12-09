19991a2d199d531023d4f628f6de991c73c2afb0
Manuel Mendez <mmendez534@gmail.com>
2019-12-09 10:21:36 -0500

    wlroots: remove unneeded mesa-19.2.2 patch

--- pkgs/development/libraries/wlroots/default.nix
+++ pkgs/development/libraries/wlroots/default.nix
@@ -15,15 +15,6 @@ stdenv.mkDerivation rec {
     sha256 = "1ak86kx617c81dy85wg9rldy1z3n8ch93cjc05a4j6sifv0nkyfm";
   };
 
-  patches = [
-    # add missing header that changed in mesa-19.2.2
-    # https://github.com/swaywm/wlroots/issues/1862
-    (fetchpatch {
-      url = "https://github.com/swaywm/wlroots/commit/d113e48a2a32542fe6e12f1759f07888364609bf.diff";
-      sha256 = "1h09j1gmnzlz4py92a92chgy8xzsd8h8xn5irq9s2hq4cla66h87";
-    })
-  ];
-
   # $out for the library and $examples for the example programs (in examples):
   outputs = [ "out" "examples" ];
 
