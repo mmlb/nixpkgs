5dc983546a29a831c11c6f11e0ce1ea0587c279c
Manuel Mendez <mmendez534@gmail.com>
2020-05-13 11:10:35 -0400

    Switch from tags to the revisions they point to

--- pkgs/applications/window-managers/sway/default.nix
+++ pkgs/applications/window-managers/sway/default.nix
@@ -8,12 +8,12 @@
 
 stdenv.mkDerivation rec {
   pname = "sway";
-  version = "1.4";
+  version = "unstable-2020-01-22g${builtins.substring 0 9 src.rev}";
 
   src = fetchFromGitHub {
     owner = "swaywm";
     repo = pname;
-    rev = version;
+    rev = "f681d529397bd6b5ab3850008857d423415999c1";
     sha256 = "11qf89y3q92g696a6f4d23qb44gqixg6qxq740vwv2jw59ms34ja";
   };
 
