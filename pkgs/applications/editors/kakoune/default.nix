de6e37527a19fc3fbd25ed889910166a7daa67c2
Manuel Mendez <mmendez534@gmail.com>
2020-07-30 17:37:42 -0400

    kakoune: Switch from tags to the revisions they point to

--- pkgs/applications/editors/kakoune/default.nix
+++ pkgs/applications/editors/kakoune/default.nix
@@ -4,16 +4,17 @@ with stdenv.lib;
 
 stdenv.mkDerivation rec {
   pname = "kakoune-unwrapped";
-  version = "2020.08.04";
+  version = "unstable-2020-08-04g${builtins.substring 0 9 src.rev}";
   src = fetchFromGitHub {
-    repo = "kakoune";
     owner = "mawww";
-    rev = "v${version}";
+    repo = "kakoune";
+    rev = "2c437cfa02ca568750182209ef6249c6975243a5";
     sha256 = "1cgkis8bywy5k8k6j4i3prikpmhh1p6zyklliyxbc89mj64kvx4s";
   };
   nativeBuildInputs = [ pkgconfig ];
   buildInputs = [ ncurses asciidoc docbook_xsl libxslt ];
   makeFlags = [ "debug=no" ];
+  enableParallelBuilding = true;
 
   postPatch = ''
     export PREFIX=$out
